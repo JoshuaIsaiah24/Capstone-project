b825cb858bb922305147e5ad95a2025ee68cbecc
JoshuaIsaiah24 <143246779+JoshuaIsaiah24@users.noreply.github.com>
2023-11-06 05:14:29 +0800

    updated with the correct settings

--- Littlelemon/Littlelemon/settings.py
+++ Littlelemon/Littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Littlelemon',
         'USER': 'root',
-        'PASSWORD': 'P@sswordyourface1234',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
