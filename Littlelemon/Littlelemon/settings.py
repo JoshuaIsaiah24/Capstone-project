3d12ab2bd2bb5fc5b5216b0980f36b534b329eac
JoshuaIsaiah24 <143246779+JoshuaIsaiah24@users.noreply.github.com>
2023-11-06 05:17:58 +0800

    removed some confidential data in the settings file

--- Littlelemon/Littlelemon/settings.py
+++ Littlelemon/Littlelemon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Littlelemon',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'a',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
